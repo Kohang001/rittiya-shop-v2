4816efd70cdd3fb2510d67b130046b495426c4ef
Kohang001 <nathwrrthsumphlxy@gmail.com>
2026-07-09 18:46:07 +0700

    Fix missing export default in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,13 @@
 import { auth } from './firebase/config';
+
 console.log(auth);
+
+function App() {
+  return (
+    <div>
+      {/* เนื้อหาเดิมของหน้า */}
+    </div>
+  );
+}
+
+export default App;
